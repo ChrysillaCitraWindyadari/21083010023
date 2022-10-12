1d0c85f0cdb3ac52ea841cc0b577fe30b12943c0
ChrysillaCitraWindyadari <citrachrysilla@gmail.com>
2022-10-12 10:51:05 +0700

    Tugas 6

---
+++ Array_CA.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#deklarasi array compound assignment
+distroLinuxDesktop=('BlankOn' 'Ubuntu' 'Debian' 'ArchLinux' 'LinuxMint')
+distroLinuxServer=('UbuntuServer' 'CentOS' 'FedoraServer')
+
+#cara mengambil nilai array
+echo ${distroLinuxDesktop[*]}
+echo ${distroLinuxServer[*]}
+
+
